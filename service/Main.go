813414fe541b2c6c5fc6cc27ba5de0ff33233036
cKeinsley <keinsleyChris@gmail.com>
2018-10-23 11:37:03 -0400

    collin wanted newline

--- service/Main.go
+++ service/Main.go
@@ -11,8 +11,8 @@ const PORT = 3000
 
 func main() {
 	log.SetFlags(log.LUTC | log.Llongfile | log.Ldate | log.Ltime)
-	log.Printf("Starting on port %d", PORT)
+	log.Printf("Starting on port %d\n", PORT)
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(PORT), router))
+	log.Fatalln(http.ListenAndServe(":"+strconv.Itoa(PORT), router))
 }
